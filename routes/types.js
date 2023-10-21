34dde99eb4b1f9d1326d32b7666c5a5ac80a8129
Shub3am <shubhamvishwakarma0604@gmail.com>
2023-10-21 13:41:39 +0530

    Added Algorithms Supported Section

--- routes/types.js
+++ routes/types.js
@@ -3,7 +3,12 @@ const crypto = require('crypto');
 const router = express.Router()
 
 router.get('/all', (req, res) => {
-    res.send(['sha-256', 'sha-1', 'md5'])
+    let algos = [
+        { algoName: "sha-256", description: "SHA-256 stands for Secure Hash Algorithm 256-bit and it's used for cryptographic security" },
+        { algoName: "sha-1", description: "SHA-1 is a cryptographic hash function that produces a 160-bit (20-byte) hash value and is commonly used in security applications and protocols." },
+        { algoName: "md5", description: "MD5, which stands for Message Digest Algorithm 5, is a widely used cryptographic hash function that produces a 128-bit (16-byte) hash value and is commonly used for data integrity checks." }
+      ];
+    res.send(algos)
 })
 
 router.get('/get/:id', (req, res) => {
