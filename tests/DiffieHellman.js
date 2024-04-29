554c59c69c0743d0edbbc88b60d02c11eb2db487
dinesh <talwadkerdinesh@gmail.com>
2024-04-30 04:49:03 +0530

    rm: tests

--- tests/DiffieHellman.js
+++
@@ -1,9 +0,0 @@
-// DiffieHellman technique for now.
-const crypto = require('crypto');
-const alice = crypto.createDiffieHellman(2048);
-const aliceKey = alice.generateKeys();
-const bob = crypto.createDiffieHellman(alice.getPrime(), alice.getGenerator());
-const bobKey = bob.generateKeys();
-const aliceSecret = alice.computeSecret(bobKey);
-const bobSecret = bob.computeSecret(aliceKey);
-console.log(aliceSecret.toString('hex'), "/n", bobSecret.toString('hex'))
