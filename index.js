8e9cc230ba53e8834ce6fe35da9168e745ca8583
dinesh <talwadkerdinesh@gmail.com>
2024-04-30 03:55:24 +0530

    fix: file paths

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const types = require('./routes/types')
-const api = require('./routes/api')
-const views = require('./routes/views')
+const types = require('./routes/types');
+const api = require('./routes/api');
+const views = require('./routes/views');
 
-app.use('/types', types)
-app.use('/api', api)
-app.use('/', views)
+app.use('/types', types);
+app.use('/api', api);
+app.use('/', views);
 
 app.listen(PORT, function (err) {
     if (err) console.log(err);
