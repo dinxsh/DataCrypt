03b70ae209e0615c385ae5cee263953c226f2a40
dinesh <talwadkerdinesh@gmail.com>
2024-04-30 12:18:26 +0530

    fix: register img sizes

--- models/user.js
+++ models/user.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, unique: true },
     email: { type: String, unique: true },
     password: String,
-    profile: String
+    profile: String,
+    dateCreated: String,
+    lastLoggedIn: String,
 });
 
 module.exports = UserSchema;
