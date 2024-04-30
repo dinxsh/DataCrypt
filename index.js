2f1c7fb9d5954f3a05b51110e317fdab2d687f1e
dinesh <talwadkerdinesh@gmail.com>
2024-04-30 10:01:39 +0530

    fix: secure cookie options

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(session({
     secret: 'uid',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false }
+    cookie: { secure: true }
 }));
 
 app.use('/types', types);
